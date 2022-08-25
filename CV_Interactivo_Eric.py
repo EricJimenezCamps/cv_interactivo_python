fa4b4b5234558cfd50301284e43c635a77428b2a
Eric AJ <ericjimenezcamps@gmail.com>
2022-08-25 20:19:57 +0200

    Creamos el menu

---
+++ CV_Interactivo_Eric.py
@@ -0,0 +1,19 @@
+def run ():
+    pass
+
+def menu ():
+    texto_menu= print("""
+    ------------ BIENVENIDO A MI CV EN PYTHON 🐍 ------------
+
+                Índice:
+                1. Perfil personal
+                2. Experiencia Laboral
+                3. Historial Académico
+                4. Herramientas, idiomas y competencias
+                5. Salir
+    
+                \033[36m * Descarga CV en PDF aquí: https://cutt.ly/zSomeNd \033[0m
+             
+                ¿Qué apartado quieres visitar?
+    
+    """)
